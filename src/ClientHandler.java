71221a2bc914688068d971b9f862433628958bfb
S0fca <sofia.hennelova@gmail.com>
2024-03-16 12:39:16 +0100

    Initial commit

---
+++ src/ClientHandler.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable {
+
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String clientUsername;
+
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            clientUsername = bufferedReader.readLine();
+            clientHandlers.add(this);
+            broadcastMessage("SERVER: " + clientUsername + " has entered the chat!");
+        } catch (IOException e) {
+            closeEverything(socket, bufferedWriter, bufferedReader);
+        }
+    }
+
+    private void broadcastMessage(String messageToSend) {
+        for (ClientHandler clientHandler : clientHandlers) {
+            try {
+                if (!clientHandler.clientUsername.equals(clientUsername)) {
+                    clientHandler.bufferedWriter.write(messageToSend);
+                    clientHandler.bufferedWriter.newLine();
+                    clientHandler.bufferedWriter.flush();
+                }
+            } catch (IOException e) {
+                closeEverything(socket, bufferedWriter, bufferedReader);
+            }
+        }
+    }
+
+    public void removeClientHandler() {
+        clientHandlers.remove(this);
+        broadcastMessage("SERVER: " + clientUsername + " has left the chat\n");
+    }
+
+    private void closeEverything(Socket socket, BufferedWriter bufferedWriter, BufferedReader bufferedReader) {
+        removeClientHandler();
+        try {
+            if (bufferedWriter != null) bufferedWriter.close();
+            if (bufferedReader != null) bufferedReader.close();
+            if (socket != null) socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void run() {
+        String messageFromClient;
+
+        while (socket.isConnected()) {
+            try {
+                messageFromClient = bufferedReader.readLine();
+                broadcastMessage(messageFromClient);
+            } catch (IOException e) {
+                closeEverything(socket, bufferedWriter, bufferedReader);
+                break;
+            }
+        }
+    }
+
+    public String getClientUsername() {
+        return clientUsername;
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private final ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() {
+        try {
+            System.out.println("Server start");
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(socket);
+                System.out.println(clientHandler.getClientUsername() + " has connected");
+
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        } catch (IOException e) {
+            closeServerSocket();
+        }
+    }
+
+    public void closeServerSocket() {
+        try {
+            if (serverSocket != null) serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1234);
+        Server server = new Server(serverSocket);
+        server.startServer();
+    }
+}
